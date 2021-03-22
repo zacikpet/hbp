1562e15ef82e3298421dc2492aedd835487a927b
Peter Žáčik <zacikpet@fit.cvut.cz>
2021-03-22 09:47:22 +0100

    Add process queue

--- clock.py
+++ clock.py
@@ -1,8 +1,13 @@
+from rq import Queue
+
+from service import update
+from worker import connection
 from apscheduler.schedulers.blocking import BlockingScheduler
 
 scheduler = BlockingScheduler()
+q = Queue(connection=connection)
 
 
 @scheduler.scheduled_job('cron', days_of_week='mon-sun', hour=11)
-def update():
-    pass
+def schedule_update():
+    q.enqueue(update)
