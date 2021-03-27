b320205eb50d6171522885689ccb479ccad2e7c5
Peter Žáčik <zacikpet@fit.cvut.cz>
2021-03-27 09:57:56 +0100

    Work on deployment

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 from datetime import timedelta
 
-from flask import Flask
+from flask import Flask, render_template, send_from_directory
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager
 from werkzeug.routing import BaseConverter
@@ -10,7 +10,7 @@ from api import api
 from commands import connect_command, fill_command, update_command, erase_command, classify_command, stats_command
 from encoders import MongoJSONEncoder, ObjectIdConverter
 
-app = Flask(__name__, static_url_path='', static_folder='static')
+app = Flask(__name__, static_url_path='/static/', static_folder='build')
 
 app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')
 app.config['JWT_COOKIE_SECURE'] = True
@@ -36,12 +36,12 @@ class RegexConverter(BaseConverter):
 
 app.url_map.converters['regex'] = RegexConverter
 
-
+'''
 @app.route('/', defaults={'path': ''})
 @app.route('/<regex(".*"):path>/')
 def root(path):
     return app.send_static_file('index.html')
-
+'''
 
 app.cli.add_command(fill_command)
 app.cli.add_command(update_command)
