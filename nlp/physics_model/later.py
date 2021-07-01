9e487be2e6df99b7f74a26342fb84ba0f26f626e
Peter Žáčik <zacikpet@fit.cvut.cz>
2021-07-01 13:06:00 +0200

    Testing

--- nlp/physics_model/later.py
+++ nlp/physics_model/later.py
@@ -4,10 +4,31 @@ from nltk.corpus import stopwords
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
-from .util import preprocess_text
+from util import preprocess_text
 
 vectorizer = pickle.load(open('vectorizer', 'rb'))
 classifier = pickle.load(open('classifier', 'rb'))
+
+
+def perf_measure(y_actual, y_pred):
+    TP = 0
+    FP = 0
+    TN = 0
+    FN = 0
+
+    for i in range(len(y_pred)):
+
+        if y_actual[i] == 'BSM' and y_pred[i] == 'BSM':
+            TP += 1
+        if y_pred[i] == 'BSM' and y_actual[i] == 'SM':
+            FP += 1
+        if y_actual[i] == 'BSM' and y_pred[i] == 'SM':
+            FN += 1
+        if y_pred[i] == 'SM' and y_pred[i] == 'SM':
+            TN += 1
+
+    return(TP, FP, TN, FN)
+
 
 with open('test.csv') as csv_file:
 
@@ -23,3 +44,4 @@ with open('test.csv') as csv_file:
 
     print(classification_report(models, prediction))
     print(accuracy_score(models, prediction))
+    print(perf_measure(models, prediction))
